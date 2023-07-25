0183b7376a2592fb2a13c25be0e07c4e3be99b48
Gustavo Costa <gustavo.costa@lojainterativa.com>
2023-07-25 18:56:37 -0300

    atualização das tabelas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FilterService, GridComponent, EditService, SortService, ToolbarService,
-  ToolbarItems, EditSettingsModel, ForeignKeyService
-} from '@syncfusion/ej2-angular-grids';
+import { FilterService, EditService, SortService, ToolbarService, ToolbarItems, EditSettingsModel } from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'app-root',
@@ -10,24 +7,55 @@ import {
   styleUrls: ['./app.component.scss'],
   providers: [FilterService, EditService, SortService, ToolbarService]
 })
+
 export class AppComponent implements OnInit{
   title = 'demo-components-syncfusion';
 
   public data: Object[] = [
     {
-      OrderID: 10248, CustomerID: 'VINET', EmployeeID: 5, OrderDate: new Date(8364186e5),
-      ShipName: 'Vins et alcools Chevalier', ShipCity: 'Reims', ShipAddress: '59 rue de l Abbaye',
-      ShipRegion: 'CJ', ShipPostalCode: '51100', ShipCountry: 'France', Freight: 32.38, Verified: !0
+      Status: 'Elaboração',
+      Documento: 'AI-TAESA-659-845',
+      Rev: 1,
+      "Documento Vinculado": 'NPC 26943',
+      "Agentes Vinculados": 'Eletrosul COT',
+      Responsável: 'João Guilherme',
+      Concessão: 'Janaúba',
+      GM: 'ATE',
+      Subestação: 'Aymorés',
+      Início: new Date(8364186e5),
+      Termino: new Date(8364186e5),
+      Resumo: 'Obra em execução',
+      Comentários: 1
     },
     {
-      OrderID: 10249, CustomerID: 'TOMSP', EmployeeID: 6, OrderDate: new Date(836505e6),
-      ShipName: 'Toms Spezialitäten', ShipCity: 'Münster', ShipAddress: 'Luisenstr. 48',
-      ShipRegion: 'CJ', ShipPostalCode: '44087', ShipCountry: 'Germany', Freight: 11.61, Verified: !1
+      Status: 'Elaboração',
+      Documento: 'AI-TAESA-659-845',
+      Rev: 1,
+      "Documento Vinculado": 'NPC 26943',
+      "Agentes Vinculados": 'Eletrosul COT',
+      Responsável: 'João Guilherme',
+      Concessão: 'Janaúba',
+      GM: 'ATE',
+      Subestação: 'Aymorés',
+      Início: new Date(8364186e5),
+      Termino: new Date(8364186e5),
+      Resumo: 'Obra em execução',
+      Comentários: 1
     },
     {
-      OrderID: 10250, CustomerID: 'HANAR', EmployeeID: 4, OrderDate: new Date(8367642e5),
-      ShipName: 'Hanari Carnes', ShipCity: 'Rio de Janeiro', ShipAddress: 'Rua do Paço, 67',
-      ShipRegion: 'RJ', ShipPostalCode: '05454-876', ShipCountry: 'Brazil', Freight: 65.83, Verified: !0
+      Status: 'Elaboração',
+      Documento: 'AI-TAESA-659-845',
+      Rev: 1,
+      "Documento Vinculado": 'NPC 26943',
+      "Agentes Vinculados": 'Eletrosul COT',
+      Responsável: 'João Guilherme',
+      Concessão: 'Janaúba',
+      GM: 'ATE',
+      Subestação: 'Aymorés',
+      Início: new Date(8364186e5),
+      Termino: new Date(8364186e5),
+      Resumo: 'Obra em execução',
+      Comentários: 1
     }
   ];
   public pageSettings!: Object;
@@ -39,7 +67,12 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.pageSettings = { pageCount: 5 };
         this.filterSettings = { type: 'Menu' };
-        this.toolbarItems = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+        this.toolbarItems = [
+          'Edit',
+          'Delete',
+          'Update',
+          'Cancel'
+        ];
         this.editOptions = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal' };
         this.orderidrules = { required: true };
   }
